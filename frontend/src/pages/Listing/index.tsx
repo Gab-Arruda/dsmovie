c15d457f0dc98710b7039578873c031231eb38e4
gasantos <gasantos@nelogica.com.br>
2022-01-15 20:20:28 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,34 @@
+import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
 
 function Listing() {
+    // usado css inline pq a merda do bootstrap não queria importar o mb-3
+    const mb = {
+        marginBottom: "16px",
+    };
     return (
-        <Pagination/>
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-4 col-lg-3" style={mb}>
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3" style={mb}>
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3" style={mb}>
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3" style={mb}>
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-md-4 col-lg-3" style={mb}>
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 export default Listing;
